9ea2ebb38389d92ab2c9dc35609d7b51332d294e
yaderp <yaderch@gmail.com>
2020-05-06 13:01:42 -0500

    v00.00

---
+++ SonicDoc/app/src/main/java/com/yaderp/sonicdoc/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yaderp.sonicdoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
